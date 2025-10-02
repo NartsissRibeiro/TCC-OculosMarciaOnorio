2c1b7f3a53dfb940ffea6de0abcf9056bdae0c4b
unknown <only.narciso042@gmail.com>
2025-10-02 19:48:17 -0300

    adicionando o banco

---
+++ config.php-example.php
@@ -0,0 +1,11 @@
+<?php 
+// codigo de configuração para o php mailler
+return [
+    'MAIL_USERNAME' => 'seu-email@gmail.com',
+    'MAIL_PASSWORD' => 'sua-senha',
+    'MAIL_HOST' => 'smtp.gmail.com',
+    'MAIL_PORT' => 587,
+    'MAIL_ENCRYPTION' => 'tls',
+    'MAIL_FROM_NAME' => 'Nome do Remetente'
+];
+?>
