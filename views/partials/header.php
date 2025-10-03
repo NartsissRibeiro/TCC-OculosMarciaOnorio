b1ed72d0022ca10955aaf7f824cc5687ca856f63
unknown <only.narciso042@gmail.com>
2025-10-03 16:47:24 -0300

    mudanças no visual

--- views/partials/header.php
+++ views/partials/header.php
@@ -13,6 +13,7 @@ if(isset($_GET['logout']) && $_GET['logout'] === 'true'){
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Márcia Onório</title>
+    <link rel="icon" href="../../assets/img/mo_logo.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <link rel="stylesheet" href="../../assets/css/style.css">
     <style>
